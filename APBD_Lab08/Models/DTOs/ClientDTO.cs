50c1ac287d92895b0c87e441cfb65756887e8368
s21485 <s21485@pjwstk.edu.pl>
2025-04-28 13:26:14 +0200

    DTOs created

---
+++ APBD_Lab08/Models/DTOs/ClientDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace APBD_Lab08.Controllers.DTOs;
+
+public class ClientDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string PESEL { get; set; }
+}
---
+++ APBD_Lab08/Models/DTOs/TripDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace APBD_Lab08.Controllers.DTOs;
+
+public class TripDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int MaxPeople { get; set; }
+}
